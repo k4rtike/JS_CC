f58cd915cc22dd47a9c10d5fecdb4686d4488865
Kartike Gupta <2k23.cs2314010@gmail.com>
2026-06-18 10:39:33 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,47 @@
+//Arrays 
+/*
+>JavaScript arrays are resizable and can contain a mix of different data types. (When those characteristics are undesirable,
+ use typed arrays instead.)
+>JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes, 
+ but must be accessed using nonnegative integers (or their respective string form) as indexes.
+>JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1, and so on — and the
+ last element is at the value of the array's length property minus 1.
+>JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects 
+create shallow copies, rather than deep copies).
+
+NOTE:A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as 
+those of the source object from which the copy was made. As a result, when you change either the source or the copy, you may 
+also cause the other object to change too. That behavior contrasts with the behavior of a deep copy, in which the source and 
+copy are completely independent.
+
+*/
+
+//Array Methods
+
+const arr=[1,2,3,4,5,6,7]
+// //console.log(typeof(arr)) // object
+// arr.push(6)
+// //console.log(arr);
+// arr.pop()
+// //console.log(arr);
+// arr.unshift(7)
+// //console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// console.log(arr.includes(3))
+// console.log(arr.indexOf(4))
+
+//const newarr= arr.join()
+// console.log(newarr);
+// console.log(typeof(newarr)) // string
+
+console.log("Orig. Array ", arr)
+
+console.log(arr.slice(2,5))
+
+console.log("After Slice: ", arr)
+
+console.log(arr.splice(2,5)) // splice(starting, count)
+
+console.log("After Splicing: ", arr)
