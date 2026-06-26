418e1f2f807764c3d0709d59e7ed72830ae51b97
Kartike Gupta <kartikegupta311@gmail.com>
2026-06-27 01:27:38 +0530

    objects

---
+++ 02_basics/03_objectgs.js
@@ -0,0 +1,11 @@
+//Object_declaration
+//singleton Object.create()
+//Object Literals
+// Object me keys and value pairs me kaam hohta hai 
+const JsUser ={ //this {} is the object
+    name : "Tiger", // name ko js string hi maan leta hai "name" aise likhne ki zarurat nhi hai. key andar value kuch bhi de sklte hain.
+    location : "Siberia",
+    email : "tigris@jungle.com",
+    isSleep : false
+}
+
